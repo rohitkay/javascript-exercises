92d80b61636f008056e608371c48681a01340448
Rohit <192195109+rohitkay@users.noreply.github.com>
2025-12-22 19:06:31 +1100

    Complete exercise repeat string

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+    let combined = "";
+    if (num < 0){
+        return "ERROR";
+    }
+    else{
+        for(let i = 0; i<num; i++){
+            combined += string;
+        }
+        return combined;
+    }
 };
 
 // Do not edit below this line
