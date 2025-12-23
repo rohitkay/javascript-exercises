478bd160e1103168b4d231af0dcc06e30bb08b72
Rohit <192195109+rohitkay@users.noreply.github.com>
2025-12-23 13:01:16 +1100

    Complete exercise sum all

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,4 +1,34 @@
 const sumAll = function(num1,num2) {
+    let largerNum = 0;
+    let smallerNum = 0;
+
+    if (num1 < 0 || num2 < 0){
+        return "ERROR";
+    }
+
+    if(num1 % 1 !=0 || num2 % 1 !=0 ){
+        return "ERROR"
+    }
+
+    if(!Number.isInteger(num1) || !Number.isInteger(num2)){
+        return "ERROR";
+    }
+
+    if (num1>num2){
+        largerNum = num1;
+        smallerNum = num2;
+    }
+    else{
+        largerNum = num2;
+        smallerNum = num1;
+    }
+    
+    let sum = 0;
+    for(let i = smallerNum; i<= largerNum; i++){
+     sum += i;   
+    }
+
+    return sum;
 
 };
 
