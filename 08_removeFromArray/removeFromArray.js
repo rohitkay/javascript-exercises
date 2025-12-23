e4f2108d1d2dc0414d67320052c140612dce9a7a
Rohit <192195109+rohitkay@users.noreply.github.com>
2025-12-23 12:27:13 +1100

    Complete exercise remove from array

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function(array, toRemove) {
+const removeFromArray = function(array, ...toRemove) {
+    toRemove.forEach(function(arg){
+        while(true){
+            const index = array.indexOf(arg);
+            if (index == -1){
+                break;
+            }
+            if (index >= -1){
+                array.splice(index,1)
+            }
+        }
+    });
+    return array;
 };
 
 // Do not edit below this line
