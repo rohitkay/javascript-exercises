eb766a63e3162384fd2084d48b711807e9a0662e
Rohit <192195109+rohitkay@users.noreply.github.com>
2026-01-07 21:12:39 +1100

    Complete findtheoldest exercise

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    let oldestAge = 0
+    let indexOfOldest = -1;
+    for(let i = 0; i<arr.length;i++){
+        if (arr[i].yearOfDeath > 0){
+            age = arr[i].yearOfDeath -arr[i].yearOfBirth;
+            
+        }
+        else{
+            age = new Date().getFullYear() - arr[i].yearOfBirth;
+            
+            
+        }
 
-};
+        if(age>oldestAge){
+                oldestAge = age
+                indexOfOldest = i;
+            }
+
+    }
+    return arr[indexOfOldest];
+}
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
