6fcc5ccdf5e67da386ee1b28df2d19aa078cf110
Rohit <192195109+rohitkay@users.noreply.github.com>
2026-01-07 20:18:38 +1100

    Complete exercise fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,29 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let firstPrev = 1;
+    let secondPrev = 0;
+    if (num == 0){
+        return 0;
+    }
+    if (num < 0){
+        return "OOPS";
+    }
+    num = +num;
+    for (let i = 1; i<= num; i++){
+        
+        
+        let currentValue = firstPrev + secondPrev
+        
+        secondPrev = firstPrev
+        console.log(secondPrev);
+        firstPrev = currentValue
+        
+    }
 
-};
+    return secondPrev;
+    
+}
+
+        
 
 // Do not edit below this line
 module.exports = fibonacci;
